d2a9b19eafbe41c791ed4a1b793d21cc084851aa
JunyanGuoGUo <guo17854229784@163.com>
2020-10-15 21:01:00 +0800

    热力图改功能（bug巨多版）

--- app/client/views/camera/Map.vue
+++ app/client/views/camera/Map.vue
@@ -9,6 +9,9 @@
       </template>
     </NavBar>
     <template>
+      <label for="">{{ location1 }}</label><br>
+      <label for="">{{ location2 }}</label><br>
+      <label for="">{{ location3 }}</label>
       <v-chart :style="{width: containerWidth + 'px',height: containerWidth + 'px'}" :options="option1"/>
       <v-chart :style="{width: containerWidth + 'px',height: containerWidth + 'px' }" :options="option2"/>
     </template>
@@ -21,7 +24,8 @@ import ECharts from 'vue-echarts'
 import 'echarts/lib/chart/line'
 import 'echarts/lib/chart/heatmap'
 
-let x0 = 30.66, y0 = 122.20, x = 30.78, y = 122.32;
+let x0 = 30.66, y0 = 122.20, x = 30.99, y = 121.87;
+//  30.33 < x < 30.99  121.87 < y < 122.53   距离大约在50km之内
 let angle = 45, dist = 3.14;
 
 let R = 6371.14;//地球半径 单位km
@@ -47,123 +51,66 @@ dist = getDistance(x0, y0, x, y);
 // 已知斜边dist，再求对边s_dist，用arcsin函数求角度
 let s_dist = getDistance(x0, y, x, y);
 angle = r_rad(Math.asin(s_dist / dist)).toFixed(2);
+var angles = 0;
+console.log(angle);
+if (x < x0 && y > y0){
+  angles = (parseFloat(angle)+90).toFixed(2);
+  console.log(angles);
+}
+else if (x < x0 && y < y0){
+  angles = (parseFloat(angle)+180).toFixed(2);
+  console.log(angles);
+}
+else if (x > x0 && y < y0){
+  angles = (parseFloat(angle)+270).toFixed(2);
+  console.log(angles);
+}
 
+let data_loc = [[30.402312, 121.98312], [30.404635, 121.9867565], [30.4076786, 121.9883725],[30.4076786, 121.9883725],
+  [30.4154325, 122.003232],[30.4159123, 122.004534],[30.454523, 122.084534],[30.459123, 122.08434],[30.557123, 122.14439]
+   [30.66423, 122.203223], [30.6631, 122.20231], [30.66231, 122.2034], [30.662311, 122.20375], [30.66232, 122.20546],
+  [30.66232, 122.20543], [30.6632, 122.20546]],
 
-let noise = () => {
+data = () => {
+  let data = [];
+  for (let i = -50; i <= 50; i++)
+    for (let j = -50; j <= 50; j++)
+      data[i, j] = 0;
 
-    let module = {};
-
-    function Grad(x, y, z) {
-      this.x = x;
-      this.y = y;
-      this.z = z;
+  for (let i = 0; i < data_loc.length; i++) {
+    let x =data_loc[i][1].toFixed(2);
+    let y =data_loc[i][0].toFixed(2);
+    let m_x = getDistance(x0,y0,x,y0);
+    let m_y = getDistance(x0,y0,x0,y);
+    m_x = Math.floor(m_x);
+    m_y = Math.floor(m_y);
+    if (x > x0 && y < y0){
+      m_y = -m_y;
     }
-
-    Grad.prototype.dot2 = function (x, y) {
-      return this.x * x + this.y * y;
-    };
-
-    Grad.prototype.dot3 = function (x, y, z) {
-      return this.x * x + this.y * y + this.z * z;
-    };
-
-    let grad3 = [new Grad(1, 1, 0), new Grad(-1, 1, 0), new Grad(1, -1, 0), new Grad(-1, -1, 0),
-      new Grad(1, 0, 1), new Grad(-1, 0, 1), new Grad(1, 0, -1), new Grad(-1, 0, -1),
-      new Grad(0, 1, 1), new Grad(0, -1, 1), new Grad(0, 1, -1), new Grad(0, -1, -1)];
-
-    let p = [151, 160, 137, 91, 90, 15,
-      131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23,
-      190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33,
-      88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166,
-      77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244,
-      102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196,
-      135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123,
-      5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42,
-      223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9,
-      129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228,
-      251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107,
-      49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254,
-      138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180];
-    // To remove the need for index wrapping, double the permutation table length
-    let perm = new Array(512);
-    let gradP = new Array(512);
-
-    // This isn't a very good seeding function, but it works ok. It supports 2^16
-    // different seed values. Write something better if you need more seeds.
-    module.seed = function (seed) {
-      if (seed > 0 && seed < 1) {
-        // Scale the seed out
-        seed *= 65536;
-      }
-
-      seed = Math.floor(seed);
-      if (seed < 256) {
-        seed |= seed << 8;
-      }
-
-      for (let i = 0; i < 256; i++) {
-        let v;
-        if (i & 1) {
-          v = p[i] ^ (seed & 255);
-        } else {
-          v = p[i] ^ ((seed >> 8) & 255);
-        }
-
-        perm[i] = perm[i + 256] = v;
-        gradP[i] = gradP[i + 256] = grad3[v % 12];
-      }
-    };
-
-    module.seed(0);
-
-    // ##### Perlin noise stuff
-    function fade(t) {
-      return t * t * t * (t * (t * 6 - 15) + 10);
+    else if (x < x0 && y < y0){
+      m_x = -m_x;
+      m_y = -m_y;
     }
-
-    function lerp(a, b, t) {
-      return (1 - t) * a + t * b;
+    else if (x < x0 && y > y0) {
+      m_x = -m_x;
     }
-
-    // 2D Perlin Noise
-    module.perlin2 = function (x, y) {
-      // Find unit grid cell containing point
-      let X = Math.floor(x), Y = Math.floor(y);
-      // Get relative xy coordinates of point within that cell
-      x = x - X;
-      y = y - Y;
-      // Wrap the integer cells at 255 (smaller integer period can be introduced here)
-      X = X & 255;
-      Y = Y & 255;
-
-      // Calculate noise contributions from each of the four corners
-      let n00 = gradP[X + perm[Y]].dot2(x, y);
-      let n01 = gradP[X + perm[Y + 1]].dot2(x, y - 1);
-      let n10 = gradP[X + 1 + perm[Y]].dot2(x - 1, y);
-      let n11 = gradP[X + 1 + perm[Y + 1]].dot2(x - 1, y - 1);
-
-      // Compute the fade curve value for x
-      let u = fade(x);
-
-      // Interpolate the four results
-      return lerp(
-        lerp(n00, n10, u),
-        lerp(n01, n11, u),
-        fade(y));
-    };
-    return module;
-  },
-  data = () => {
-    let data = [];
-    for (let i = -100; i <= 100; i++) {
-      for (let j = -100; j <= 100; j++) {
-        data.push([i, j, noise().perlin2(i / 40, j / 20) + 0.5]);
+    data[m_x,m_y] += 1;
+    //求二维数组最大值
+    var max = 0;
+    for(let i=-50;i<50;i++)
+      for(let j=-50;j<50;j++){
+        if(data[i][j]>max)
+          max = data[i][j];
       }
-    }
-    return data;
   }
 
-noise().seed(Math.random())
+  let final_data = [];
+  //数组归一化
+  for(let i=-50;i<50;i++)
+    for(let j=-50;j<50;j++)
+      final_data.push([i,j,data[i][j]/max]);
+  return final_data;
+}
 
 export default {
   name: "Map",
@@ -174,15 +121,13 @@ export default {
   data() {
     return {
       containerWidth: window.screen.width,
+      location1 : '1号设备海上定位',
+      location2 : '标准位置: ' + x0 + '°N,' + y0 + '°E',
+      location3 : '实际位置: ' + x + '°N,' + y + '°E',
       // 定义图表，各种参数
       option1: {
-        title: {
-          text: '1号设备海上定位\n' +
-            '标准位置: ' + x0 + '°N,' + y0 + '°E\n' +
-            '实际位置: ' + x + '°N,' + y + '°E'
-        },
         radiusAxis: {
-          max: 20
+          max: 50
         },
         polar: {
           center: ['50%', '50%']
@@ -199,35 +144,38 @@ export default {
         },
         series: [{
           coordinateSystem: 'polar',
+          radius: '80%',
           name: '偏移度',
           type: 'scatter',
-          data: [[0, 0], [dist, angle]]
+          data: [[0, 0], [dist, angles]]
         }]
       },
       option2: {
         tooltip: {},
         xAxis: {
           type: 'value',
-          max: 100,
-          min: -100
+          max: 50,
+          min: -50
         },
         yAxis: {
           type: 'value',
-          max: 100,
-          min: -100
+          max: 50,
+          min: -50
         },
         visualMap: {
           min: 0,
           max: 1,
-          calculable: true,
-          realtime: false,
           inRange: {
-            color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
+            color: ['#4575b4', '#74add1', '#abd9e9', '#ffffbf',
+              '#fee090', '#fdae61', '#f46d43', '#d73027',]
           }
         },
         series: [{
           name: 'Gaussian',
           type: 'heatmap',
+          itemStyle: {
+            opacity : 0.6
+          },
           data: data(),
           emphasis: {
             itemStyle: {
@@ -235,8 +183,8 @@ export default {
               borderWidth: 1
             }
           },
-          progressive: 1000,
-          animation: false
+          progressive: 100, //渲染频率
+          animation: false //动画
         }]
       }
     }
