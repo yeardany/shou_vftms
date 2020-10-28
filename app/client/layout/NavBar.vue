4cee1e41c65263fd5fc1f1f5d37d55f67df006ab
苏亦铭 <yeardany@hotmail.com>
2020-10-28 22:04:50 +0800

    恢复阿俊删除的必要组件

---
+++ app/client/layout/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <van-nav-bar :border="false" @click-left="back" @click-right="method(methodName)">
+    <template #left>
+      <slot name="left"></slot>
+    </template>
+    <template #title>
+      <slot name="title"></slot>
+    </template>
+    <template #right>
+      <slot name="right"></slot>
+    </template>
+  </van-nav-bar>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {}
+  },
+  props: {
+    methodName: String,
+    required: true
+  },
+  methods: {
+    back() {
+      this.$router.push('/home')
+    },
+    method(name) {
+      switch (name) {
+        case 'newInspect':
+          this.$router.push('/newInspect')
+          break;
+        default:
+      }
+    }
+  },
+  computed: {}
+}
+</script>
+
+<style scoped>
+
+</style>
