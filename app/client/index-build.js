44f29b31761c8e050ce4c4c6dbf6f6580ad0b13c
苏亦铭 <yeardany@hotmail.com>
2020-11-07 17:14:46 +0800

    修正

--- app/client/index-build.js
+++ app/client/index-build.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vant from 'vant'
+import VueSocketIO from 'vue-socket.io'
 import App from './app.vue'
 import createRouter from './router/router'
+import api from '../config/api'
 import 'vant/lib/index.css'
 
 import {Notify, Dialog} from 'vant';
 
 Vue.use(VueRouter)
 Vue.use(Vant);
+Vue.use(new VueSocketIO({
+  debug: false,
+  connection: api.socketAddress
+}))
 
 const router = createRouter()
 
