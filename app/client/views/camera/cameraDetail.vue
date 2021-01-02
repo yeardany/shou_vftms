4fac830b6250a147c7eb3cbc61b745aee5008e92
苏亦铭 <yeardany@hotmail.com>
2021-01-02 17:57:17 +0800

    加入监控（未完成）

--- app/client/views/camera/cameraDetail.vue
+++ app/client/views/camera/cameraDetail.vue
@@ -10,7 +10,7 @@
     </NavBar>
     <van-row>
       <van-col span="24">
-        <video src="https://v-cdn.zjol.com.cn/276990.mp4" controls muted autoplay></video>
+        <div id="video-container"></div>
       </van-col>
     </van-row>
   </div>
@@ -18,17 +18,27 @@
 
 <script>
 import NavBar from '../../layout/NavBar.vue'
+import EZUIKit from 'ezuikit-js';
 
 export default {
   name: "cameraDetail",
   components: {
     NavBar
+  },
+  mounted() {
+    var player = new EZUIKit.EZUIKitPlayer({
+      id: 'video-container',
+      accessToken: 'at.0j60sxlk8qdulp60dmfu4iq37co4zqpz-58fuatumre-11wpg7m-qicy7zinq',
+      url: 'ezopen://open.ys7.com/109715580/1.hd.live',
+      autoplay: false,
+      width: window.screen.width,
+      height: window.screen.width / 1280 * 720,
+      template: 'standard',
+      header: ['save', 'zoom', 'capturePicture']
+    })
   }
 }
 </script>
 
 <style scoped>
-video {
-  width: 100%;
-}
 </style>
--- app/client/views/camera/fishCamera.vue
+++ app/client/views/camera/fishCamera.vue
@@ -1,11 +1,4 @@
 <template>
-  <!--<van-grid :gutter="10" :column-num="3" :clickable="true">
-    <van-grid-item icon="tv-o" v-for="item in list"
-                   :key="item.id"
-                   :text="item.name"
-                   to="/cameraDetail">
-    </van-grid-item>
-  </van-grid>-->
   <div>
     <div class="title">监测区域分布</div>
     <v-chart class="mapChart" :style="chartStyle" manual-update ref="myecharts"/>
@@ -27,16 +20,6 @@ export default {
   },
   data() {
     return {
-      /*list: [
-        {id: '0', name: '渔排2号'},
-        {id: '1', name: '渔排1号'},
-        {id: '2', name: '福鲍1号'},
-        {id: '3', name: '福鲍2号'},
-        {id: '4', name: '福鲍3号'},
-        {id: '5', name: '福鲍5号'},
-        {id: '6', name: '渔排3号'},
-        {id: '7', name: '渔排4号'}
-      ]*/
       chartStyle: {
         height: window.screen.height - 150 + 'px',
         width: window.screen.width + 'px'
@@ -57,15 +40,9 @@ export default {
           trigger: "item",
           triggerOn: 'click',
           formatter: (params) => {
-            this.$dialog.confirm({
-              title: '监控详情',
-              message: '獐子岛-' + params.name + '\n' + params.value[0] + '°E, ' + params.value[1] + '°N',
-              confirmButtonText: '查看监控'
-            }).then(() => {
+            let r = confirm('獐子岛-' + params.name + '\n' + '位置：' + params.value[0] + '°E, ' + params.value[1] + '°N' + '\n' + '是否查看监控？');
+            if (r === true)
               this.$router.push('/cameraDetail')
-            }).catch(() => {
-              console.log('取消')
-            })
           }
         },
         geo: {
@@ -73,7 +50,7 @@ export default {
           layoutCenter: ['20%', '50%'],
           layoutSize: 2200,
           roam: true,
-          center: [120, 39],
+          center: [122.5, 39],
           scaleLimit: {
             min: 2,
             max: 6
@@ -117,21 +94,6 @@ export default {
   mounted() {
     const chart = this.$refs.myecharts
     chart.mergeOptions(this.option, true)
-  },
-  methods: {
-    /*convertData(data) {
-      let res = [];
-      for (let i = 0; i < data.length; i++) {
-        let geoloc = this.geoMap[data[i].name];
-        if (geoloc) {
-          res.push({
-            name: data[i].name,
-            value: geoloc.concat(data[i].value)
-          });
-        }
-      }
-      return res;
-    }*/
   }
 };
 
